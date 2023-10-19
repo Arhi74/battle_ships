c3c93dad8f520eb55c75fb6bffdca52423c6d4c4
Arhi74 <alexey.andryushin@mail.ru>
2023-10-19 22:12:11 +0500

    Добавлены исключения. Продолжается работа над классами внутренней логики.

--- exceptions.py
+++ exceptions.py
@@ -1,4 +1,4 @@
-class BoardOutException(Exception):
+class ShotOutException(Exception):
 
     def __init__(self, *args):
         if args:
@@ -26,3 +26,33 @@ class DotStatusException(Exception):
             return 'DotStatusException, {0} '.format(self.message)
         else:
             return 'Неправильно указан статус у точки!'
+
+
+class ShipBowDotException(Exception):
+
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'ShipBowDotException, {0} '.format(self.message)
+        else:
+            return 'Неверно указана начальная точка корабля!'
+
+
+class AddShipOutException(Exception):
+
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'AddShipOutException, {0} '.format(self.message)
+        else:
+            return 'Точка корабля вышла за пределы доски!'
--- internal_logic_classes.py
+++ internal_logic_classes.py
@@ -1,14 +1,16 @@
 from exceptions import *
 
+# TODO: подумать над созданием класса статусов точек
+# class DotStatus:
+#
 
 class Dot:
-
     dot_status_dict = {
-        'empty': 'О',
-        'ship': '■',
-        'hit': 'X',
-        'miss': 'T',
-        'contour': '•'
+        'empty': ' О ',
+        'ship': ' ■ ',
+        'hit': ' X ',
+        'miss': ' T ',
+        'contour': ' • '
     }
 
     _status = 'empty'
@@ -25,6 +27,11 @@ class Dot:
     def status(self, value):
         if value not in self.dot_status_dict.keys():
             raise DotStatusException
+        else:
+            self._status = value
+
+    def get_status(self):
+        return self._status
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
@@ -34,30 +41,54 @@ class Dot:
 
 
 class Ship:
-    # Длина.
-    # Точка, где размещён нос корабля.
-    # Направление корабля(вертикальное / горизонтальное).
-    # Количеством жизней(сколько точек корабля ещё не подбито).
-    def dots(self):
-        # Возвращает список всех точек корабля
-        pass
+
+    def __init__(self, size, dot_tuple):
+        # self.size = size
+        # self.bow_dot = bow_dot,
+        # self.position = position
+        self.health = size
+        self.dot_tuple = dot_tuple
+
+    # def dots(self):
+    #     # Возвращает список всех точек корабля
+    #     pass
 
 
 class Board:
 
-    # Двумерный список, в котором хранятся состояния каждой из клеток.
-    # Список кораблей доски.
-    # Параметр hid типа bool — информация о том, нужно ли скрывать корабли на доске (для вывода доски врага) или нет (для своей доски).
-    # Количество живых кораблей на доске.
-
     def __init__(self, board_size, hid):
         self.board_size = board_size
         self.hid = hid
-        self.dots_list = [Dot(x, y) for y in range(1, board_size) for x in range(1, board_size)]
+        self.dots_list = [Dot(x, y) for y in range(1, board_size + 1) for x in range(1, board_size + 1)]
+        self.ships_list = []
 
     # Метод add_ship, который ставит корабль на доску (если ставить не получается, выбрасываем исключения).
-    def add_ship(self):
-        pass
+    def add_ship(self, vertically, size, **coordinates):
+
+        dot_list = []
+        x, y = coordinates.get('x'), coordinates.get('y')
+        bow_dot = self.find_dot(x, y)
+
+        if not bow_dot:
+            raise ShipBowDotException
+
+        dot_list.append(bow_dot)
+        for shift in range(1, size):
+            if vertically:
+                dot = self.find_dot(x + shift, y)
+            else:
+                dot = self.find_dot(x, y + shift)
+
+            if not dot:
+                raise AddShipOutException
+
+            dot_list.append(dot)
+
+        else:
+            for dot in dot_list:
+                dot.status = 'ship'
+            new_ship = Ship(size, tuple(dot_list))
+            self.ships_list.append(new_ship)
 
     # Метод contour, который обводит корабль по контуру. Он будет полезен и в ходе самой игры, и в при расстановке
     # кораблей (помечает соседние точки, где корабля по правилам быть не может).
@@ -69,21 +100,28 @@ class Board:
     def print_board(self):
 
         # Печать первой строки координат
-        for x in range(1, self.board_size + 1):
+        for x in range(self.board_size + 1):
 
             if x == 0:
-                print(' ', end='|')
+                print('   ', end='|')
                 continue
 
-            print(x, end='|')
+            print(f' {x} ', end='|')
 
         print()
 
-        for x in range(1, self.board_size):
-            print(x + 1, end='|')
+        for x in range(1, self.board_size + 1):
 
-            for y in range(1, self.board_size):
-                print(self.find_dot(x, y), end='|')
+            print(f' {x} ', end='|')
+
+            for y in range(1, self.board_size + 1):
+                dot = self.find_dot(x, y)
+
+                # Скрываем корабли противника на доске
+                if self.hid and dot.get_status() == 'ship':
+                    print(dot.dot_status_dict.get('empty'), end='|')
+                else:
+                    print(dot, end='|')
 
             print()
 
@@ -95,29 +133,24 @@ class Board:
             if dot == searched_dot:
                 return dot
         else:
-            raise BoardOutException
+            return None
 
-
-    # Метод out, который для точки (объекта класса Dot) возвращает True, если точка выходит за пределы поля, и False, если не выходит.
-    def out(self, dot):
-
-        if dot in self.dots_list:
-            return False
-        else:
-            return True
-
-    # Метод, который делает выстрел по доске (если есть попытка выстрелить за пределы и в использованную точку, нужно выбрасывать исключения).
+    # Метод, который делает выстрел по доске (если есть попытка выстрелить за пределы и в использованную точку,
+    # нужно выбрасывать исключения).
     def shot(self, x, y):
 
         shot = self.find_dot(x, y)
 
-        print('Выстрел удачный!')
-        shot.status = 'miss'
+        if not shot:
+            raise ShotOutException
 
+        # if shot.get_status() == Dot.dot_status_dict.get('empty')
+        # shot.status = 'miss'
 
 
 if __name__ == "__main__":
     new_board = Board(6, False)
     new_board.print_board()
     new_board.shot(1, 1)
+    new_board.add_ship(True, 3, x=2, y=2)
     new_board.print_board()
